0da2960ced03428730da59851e74dd0fc5ff99d6
Alex Church <alex@church.id.au>
2022-12-06 07:00:09 +1100

    Added support for disabling automatic stripping of whitespace when processing input, for inputs where leading/trailing whitespace is important.

--- puzzles/aoc.py
+++ puzzles/aoc.py
@@ -7,11 +7,18 @@ import sys
 class Puzzle:
     
     # The delimiter to use to separate the input data into a list for subsequent
-    # processing. E.g. '\n', ',', etc. Delimited items can be processed prior to
-    # being added to the input list by overriding _process_input_item().
-    # Set to None to read the data in whole. In this case, data can be processed
-    # by overriding _process_input_data().
+    # processing. E.g. '\n', ',', etc. Set to None to read the data in whole.
+    # Delimited items can be processed prior to being added to the input list by
+    # overriding _process_input_item().
+    # The resulting list, or the raw data if `input_delimiter = None`, can be
+    # processed by overriding _process_input_data().
     input_delimiter = '\n'
+    
+    # By default, strip leading/trailing whitespace from each processed input
+    # item (as dictated by `input_delimiter`), or from the whole input itself
+    # (when `input_delimiter = None`). Can be disabled for puzzle inputs in
+    # which leading/trailing whitespace is important.
+    strip_whitespace = True
     
     def __init__(self, sample=False, verbosity=2):
         
@@ -25,6 +32,13 @@ class Puzzle:
         
         return os.path.join(path, filename)
     
+    def parse_string(self, value):
+        
+        if self.strip_whitespace:
+            value = value.strip()
+        
+        return value
+    
     def process_input_item(self, input_line):
         
         return input_line
@@ -37,25 +51,26 @@ class Puzzle:
         
         input_file = self.get_input_file_name()
         delimiter = self.input_delimiter
+        parse = self.parse_string
         process_item = self.process_input_item
         
         with open(input_file, 'r') as f:
             if delimiter == '\n':
-                # Trim whitespace from and process each line in the input file,
-                # skipping any blank lines
+                # Optionally trim whitespace from and process each line in the input
+                # file, skipping any blank lines
                 input_data = []
                 
                 for line in f.readlines():
-                    line = line.strip()
+                    line = parse(line)
                     if line:
                         input_data.append(process_item(line))
             else:
-                input_data = f.read().strip()  # trim whitespace (e.g. newlines)
+                input_data = parse(f.read())  # optionally trim whitespace (e.g. newlines)
                 
                 if delimiter:
-                    # Trim whitespace from and process each item in the raw
-                    # input data after applying the configured delimiter
-                    input_data = [process_item(item.strip()) for item in input_data.split(delimiter)]
+                    # Optionally trim whitespace from and process each item in the
+                    # raw input data after applying the configured delimiter
+                    input_data = [process_item(parse(item)) for item in input_data.split(delimiter)]
                 
                 # Apply any overall processing of the input data
                 input_data = self.process_input_data(input_data)
